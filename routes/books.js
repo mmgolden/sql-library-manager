d0a318208ae76eaf1267537570fe422671b95525
Melinda Golden <info@melindagolden.com>
2019-02-24 17:45:43 -0500

    Got the full list of books in books.js

--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const Book = require('../models').Book;
 
 // Create new router
 const router = express.Router();
 
 // Show the full list of books
 router.get('/', (req, res) => {
-    res.render('index');
+    Book.findAll({ order: [["title", "ASC"]] })
+        .then((books) => {
+            res.locals.books = books;
+            res.render('index');
+        })
+        .catch((err) => {
+            res.sendStatus(500);
+        });
 });
 
 // Export router
