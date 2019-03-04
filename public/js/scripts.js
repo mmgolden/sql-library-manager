050c99f911ee9dece4efcb809fab496330395b4c
Melinda Golden <info@melindagolden.com>
2019-03-03 21:28:29 -0500

    Added pagination and search

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,12 +2,14 @@ const modalContainer = document.querySelector('.modal-container');
 const deleteBtn = document.getElementById('delete-btn');
 const closeBtn = document.querySelector('.close-btn');
 
+// Open modal
 if (deleteBtn) {
     deleteBtn.addEventListener('click', function() {
         modalContainer.style.display = 'block';
     });
 }
 
+// Close modal
 if (closeBtn) {
     closeBtn.addEventListener('click', function() {
         modalContainer.style.display = 'none';
--- routes/books.js
+++ routes/books.js
@@ -1,15 +1,105 @@
 const express = require('express');
 const Book = require('../models').Book;
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 // Create new router
 const router = express.Router();
 
 // Show the full list of books
 router.get('/', (req, res) => {
-    Book.findAll({ order: [["title", "ASC"]] })
+
+    // Calculate what the offset should be for pagination
+    const calculateOffset = (pageNum) => {
+        if (pageNum === undefined || pageNum <= 1) {
+            return 0;
+        } else {
+            return (pageNum - 1) * 10;
+        }
+    };
+
+    let numOfPages;
+    const offset = calculateOffset(req.query.page);
+    const limit = 10;
+
+    // Get the total book count and calculate the number of pages for pagination
+    Book.count()
+        .then((count) => {
+            numOfPages = Math.ceil(count / limit);
+        });
+
+    // Get all of the books
+    Book.findAll({ 
+            order: [["title", "ASC"]],
+            offset,
+            limit
+        })
         .then((books) => {
-            res.locals.books = books;
-            res.render('index');
+            res.render('index', {
+                books,
+                numOfPages
+            });
+        })
+        .catch((err) => {
+            res.sendStatus(500);
+        });
+});
+
+// Search
+router.post('/', (req, res) => {
+    // Calculate what the offset should be for pagination
+    const calculateOffset = (pageNum) => {
+        if (pageNum === undefined || pageNum <= 1) {
+            return 0;
+        } else {
+            return (pageNum - 1) * 10;
+        }
+    };
+
+    let numOfPages;
+    const offset = calculateOffset(req.query.page);
+    const limit = 10;
+    const search = req.body.search;
+
+    // Get the books that match the search
+    Book.findAll({ 
+            where: {
+                [Op.or]: [
+                    {
+                        title: {
+                            [Op.like]: `%${search}%`
+                        }
+                    },
+                    {
+                        author: {
+                            [Op.like]: `%${search}%`
+                        }
+                    },
+                    {
+                        genre: {
+                            [Op.like]: `%${search}%`
+                        }
+                    },
+                    {
+                        year: {
+                            [Op.like]: `%${search}%`
+                        }
+                    }
+                ]
+            },
+            offset,
+            limit
+        })
+        .then((books) => {
+            console.log(books);
+            if (books.length > 0) {
+                res.render('index', {
+                    books,
+                    numOfPages
+                });
+            } else {
+                res.render('no-results');
+            }
         })
         .catch((err) => {
             res.sendStatus(500);
